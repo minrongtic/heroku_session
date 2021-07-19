f51e10b232c4dc6d7ddf8b9eaa29dc13dbb4aee8
MINYOUNG <minyoung@MINYOUNGui-MacBookPro.local>
2021-07-19 19:36:06 +0900

    heroku_session4

--- selfproject/settings.py
+++ selfproject/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog', 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../blog/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'blog', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
